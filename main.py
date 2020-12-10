02101323a005f88cd66b2d1fa614f82f6e69bcbf
VoidWhysper <mailerxxdaemon@gmail.com>
2020-12-10 23:13:01 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import csv
+from day1 import *
+
+#Day 1 solutions
+challenge1_2in(BenInput,"Ben")
+challenge1_3in(BenInput,"Ben")
+challenge1_2in(JamesInput,"James")
+challenge1_3in(JamesInput,"James")
